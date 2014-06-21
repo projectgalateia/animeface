2ddf4f100761ce1935b393104a19c2751754f77a
pygmalion <pygmalion@projectgalateia.net>
2014-06-21 23:18:46 +0900

    Initial Fork

---
+++ src/nv_num_lapack.h
@@ -0,0 +1,18 @@
+#ifndef __NV_NUM_LAPACK_H
+#define __NV_NUM_LAPACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "f2c.h"
+#include "clapack/clapack.h"
+
+// ilaenv
+integer ilaenv_(integer *, char *, char *, integer *, integer *, 
+				integer *, integer *);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
